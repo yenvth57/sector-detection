428395aac99d2b373b4ad7636ee4285b886c3734
k2pctdn <quangkhai338@gmail.com>
2018-11-06 01:56:27 +0900

    2nd version

--- sector-detection.py
+++ sector-detection.py
@@ -1,4 +1,7 @@
-import pandas as pd
-x = 'T01'
-df = pd.read_csv("input.csv", sep = ",")
-print(df[df["sectorname"].str.contains("T01", na=False)])
+import csv
+sector_name: str = input('Please input a sector name: ')
+with open('input.csv', 'r') as sector:
+    r = csv.reader(sector)
+    for i in range(len(r)):
+        if sector_name in r[i][13]:
+            print(r[i])
